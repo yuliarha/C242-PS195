e587ec3c6c2995ab61aa49da49628783facfa7e6
Novriandika <novriandika15@gmail.com>
2024-11-11 21:29:12 +0700

    Register Api

--- src/handler.js
+++ src/handler.js
@@ -1,32 +1,51 @@
 const dbConfig = require('./db')
+const bcrypt = require('bcrypt')
 
-const test = async(request, h) => {
+const registerUser = async(request, h) => {
 const { 
    id,
   username,
   email,
-  hashed_password,
+  password,
   role } = request.payload
   const created_at = new Date()
-  const updated_at = new Date()
+  const updated_at = created_at
+
+  const salt = await bcrypt.genSalt(10)
+  const hashed_password = await bcrypt.hash(password, salt)
 
   const createQuery =
   'INSERT INTO users (id, username, email, hashed_password, role, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?, ?)'
-  const [result] = await dbConfig.query(createQuery, [
-    id,
-    username,
-    email,
-    hashed_password,
-    role,
-    created_at,
-    updated_at,
-  ])
-  return h.response ({
-    status : 'success',
-    result
-
+  try {
+    const [result] = await dbConfig.query(createQuery, [
+      id,
+      username,
+      email,
+      hashed_password,
+      role,
+      created_at,
+      updated_at,
+    ])
+  const response = h.response ({
+    data: result,
   })
+  response.code(200)
+  return response
+  } catch (error) {
+    if (error.code === 'ER_DUP_ENTRY'){
+      const response = h.response({
+        data: "Username atau Email Sudah Ada"
+      })
+      response.code(400)
+      return response
+    }
+    const response = h.response ({
+      data: error
+    })
+    response.code(500)
+    return response
+  }
 }
 
 
-module.exports = {test};
+module.exports = {registerUser};
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
-const { test } = require("./handler");
+const { registerUser } = require("./handler");
 
 
 const routes = [
     {
       method: 'POST',
-      path: '/api-test',
-      handler: test
+      path: '/api/user/register',
+      handler: registerUser
     
     },
 
 ]
 
-module.exports = routes;
+module.exports = routes
