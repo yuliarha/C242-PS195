ed51dc9d486bc198e75697e572fc868f4c96b0e7
John Jovi Sidabutar <sidabutarjohnjovi@gmail.com>
2024-12-05 00:14:17 +0700

    added recommend by last seen

--- main.py
+++ main.py
@@ -70,5 +70,23 @@ def recommendPlaceByLocation(location):
     return jsonify(result)
 
 
+@app.route("/api/destination/recommendation-cb/history/<lastseen_id>", methods=["GET"])
+def recommendPlaceByLastseen(lastseen_id):
+    top_n = 100
+    place_names = get_similar_places(int(lastseen_id), top_n)
+    place_names_str = "', '".join(place_names)
+    query = f"SELECT * FROM destinations WHERE place_name IN ('{place_names_str}');"
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]
+    result = [dict(zip(columns, row)) for row in rows]
+    cursor.close()
+    connection.close()
+
+    return jsonify(result)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
