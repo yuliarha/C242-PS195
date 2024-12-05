9340a00a6f643fb5fe2cacb903c5bb829c6db998
John Jovi Sidabutar <sidabutarjohnjovi@gmail.com>
2024-12-05 12:19:49 +0700

    edited README

--- README.md
+++ README.md
@@ -5,11 +5,18 @@
 ## Description
 
 ## API Documentation
-![register](https://github.com/user-attachments/assets/8e52208a-db21-4113-9383-68d05cc072b7)
 
-![login drawio](https://github.com/user-attachments/assets/c4eba395-32ee-45e9-b4a3-aedd10f55afb)
+### Register User
 
+| **Method**     | **POST**                         |
+| -------------- | -------------------------------- |
+| `Endpoint`     | `/api/user/register`             |
+| -------------- | -------------------------------- |
+| `Headers`      | `Content-Type: application/json` |
+| -------------- | -------------------------------- |
+| `Request Body` | `JSON`                           |
+| -------------- | -------------------------------- |
 
 ## Cloud Architecture
-![hotrip architecture drawio (6)](https://github.com/user-attachments/assets/051882ee-3d5a-4981-91c9-3ea2eb106b96)
 
+![hotrip architecture drawio (6)](https://github.com/user-attachments/assets/051882ee-3d5a-4981-91c9-3ea2eb106b96)
--- src/routes.js
+++ src/routes.js
@@ -6,46 +6,46 @@ const {
   recommendPlaceByLastseen,
   getDestinationByCatergoryName,
   getDestinationByCityTag,
-} = require("./handler")
-const { validateToken } = require("./token")
+} = require('./handler')
+const { validateToken } = require('./token')
 
 const routes = [
   {
-    method: "POST",
-    path: "/api/user/register",
+    method: 'POST',
+    path: '/api/user/register',
     handler: registerUser,
   },
   {
-    method: "POST",
-    path: "/api/user/login",
+    method: 'POST',
+    path: '/api/user/login',
     handler: loginUser,
   },
   {
-    method: "GET",
-    path: "/api/destination/place/{id}",
+    method: 'GET',
+    path: '/api/destination/place/{id}',
     handler: getDestinationById,
   },
   {
-    method: "POST",
-    path: "/api/destination/search",
+    method: 'POST',
+    path: '/api/destination/search',
     handler: searchDestinationByPlaceName,
   },
   {
-    method: "GET",
-    path: "/api/destination/recommend/history",
+    method: 'GET',
+    path: '/api/destination/recommend/history',
     handler: recommendPlaceByLastseen,
     options: {
       pre: [{ method: validateToken }],
     },
   },
   {
-    method: "GET",
-    path: "/api/destination/category/{categoryname}",
+    method: 'GET',
+    path: '/api/destination/category/{categoryname}',
     handler: getDestinationByCatergoryName,
   },
   {
-    method: "GET",
-    path: "/api/destination/city/{citytag}",
+    method: 'GET',
+    path: '/api/destination/city/{citytag}',
     handler: getDestinationByCityTag,
   },
 ]
